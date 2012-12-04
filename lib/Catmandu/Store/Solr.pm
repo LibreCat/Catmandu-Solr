0583097b76abc68dc2b583f27540384e721b726b
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2012-12-04 14:26:45 +0100

    version

--- lib/Catmandu/Store/Solr.pm
+++ lib/Catmandu/Store/Solr.pm
@@ -13,11 +13,11 @@ Catmandu::Store::Solr - A searchable store backed by Solr
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
